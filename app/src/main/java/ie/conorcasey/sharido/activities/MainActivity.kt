49a60bd15c7a37ac2df8aa34abeba134d0cb4de7
Conor Casey <conorcasey258@gmail.com>
2021-02-11 13:57:42 +0000

    FYP-5 Jira test 2

--- app/src/main/java/ie/conorcasey/sharido/activities/MainActivity.kt
+++ app/src/main/java/ie/conorcasey/sharido/activities/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : AppCompatActivity() {
     //sign_out.setOnClickListener(this)
   }
 
-
+//FYP JIRA TEST 2
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
     // Inflate the menu; this adds items to the action bar if it is present.
     menuInflater.inflate(R.menu.main, menu)
